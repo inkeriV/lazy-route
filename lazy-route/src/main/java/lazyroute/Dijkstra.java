c291cb0a05dd442b5f9db928a8a77666e4943b3a
Virkki Inkeri A <vbinkeri@lx7-fuxi352.ad.helsinki.fi>
2018-08-05 15:18:10 +0300

    kommentit koodiin

--- lazy-route/src/main/java/lazyroute/Dijkstra.java
+++ lazy-route/src/main/java/lazyroute/Dijkstra.java
@@ -15,106 +15,123 @@ import java.util.Stack;
  */
 
 public class Dijkstra implements Comparable<Dijkstra> {
+    
+    /* Dijkstra*/
 
-        private int id;
-        private int paino;
-        private int dist;
+    private int id;
+    private int paino;
+    private int dist;
+    
+    //konstruktori oliolle    
+    public Dijkstra(int nid, int npaino, int ndist) {
+        id = nid;
+        paino = npaino;
+        dist = ndist;
+    }
+    
+    
+    // SET ja GET metodit 
+    public void setId(int nid) { this.id = nid; }
+    public void setPaino(int npaino) { this.paino = npaino; }
+    public void setDist(int ndist) { this.dist = ndist; }
         
-        public Dijkstra(int nid, int npaino, int ndist) {
-            id = nid;
-            paino = npaino;
-            dist = ndist;
+    public int getId() { return this.id; }
+    public int getPaino() { return this.paino; }
+    public int getDist() { return this.dist; }
+        
+    
+    
+    /* compareTo metodi solmuille minimikekoa varten. Järjestetään painon ja etäisyyden summan mukaan. */
+    @Override
+    public int compareTo(Dijkstra a) { 
+        if (dist+paino<a.dist+a.paino) {
+            return -1;
         }
-        
-        public void setId(int nid) { this.id = nid; }
-        public void setPaino(int npaino) { this.paino = npaino; }
-        public void setDist(int ndist) { this.dist = ndist; }
-        
-        public int getId() { return this.id; }
-        public int getPaino() { return this.paino; }
-        public int getDist() { return this.dist; }
-        
-        @Override
-        public int compareTo(Dijkstra a) { 
-            if (dist+paino<a.dist+a.paino) {
-                return -1;
-            }
-            if (dist+paino>a.dist+a.paino) {
-                return 1;
-            }
-            return 0;
+        if (dist+paino>a.dist+a.paino) {
+            return 1;
         }
+        return 0;
+    }
+    
+    // yksittäisen solmun tulostusta varten
+    @Override
+    public String toString() {
+        return  id+", "+paino+", "+dist;
+    }
+    
+    // minimikeko
+    static PriorityQueue<Dijkstra> keko = new PriorityQueue<Dijkstra>();
         
-        @Override
-        public String toString() {
-            return  id+", "+paino+", "+dist;
-        }
+    
+    /* Dijkstra-metodin parametrit: 
+    lista - Solmut listassa ruudukon mittojen mukaan rivi kerrallaan
+    vl - vieruslista muodossa vl[int i]=lista solmu-olioita
+    alku ja loppu - solmujen järjestysnumerot */
+    
+    public static void dijkstra(Dijkstra[] lista, ArrayList<Dijkstra>[] vl, int alku, int loppu) {
         
-        static PriorityQueue<Dijkstra> keko = new PriorityQueue<Dijkstra>();
+        //alkusolmun etäisyys nollaksi 
+        lista[alku].setDist(0);
         
-        public static void addKeko(Dijkstra a) {
-            keko.add(a);
-        }
-        public static void printKeko() {
-            while (!keko.isEmpty()) {
-                Dijkstra a = keko.poll();
-                System.out.println(a);
-            }
-        }
-        public static void peekKeko() {
-            Dijkstra a = keko.peek();
-        }
-        
-        public static void dijkstra(Dijkstra[] lista, ArrayList<Dijkstra>[] vl, int alku, int loppu) {
-            lista[alku].setPaino(0);
-            lista[alku].setDist(0);
+        //lista käsiteltyjen solmujen kirjanpitoa varten
+        boolean[] checked = new boolean[lista.length];
+        for (int i=0; i<lista.length; i++) {
             
-            boolean[] checked = new boolean[lista.length];
-            for (int i=0; i<lista.length; i++) {
-
-                checked[i]=false; 
-
-            }
-            for (Dijkstra solmu: lista) {
-                keko.add(solmu);
-            }
-           
-            int[] reitti = new int[lista.length];
+            checked[i]=false; //aluksi yhtäkään solmua ei ole käsitelty
+        }
+        
+        //kaikki solmuoliot minimikekoon
+        for (Dijkstra solmu: lista) {
+            keko.add(solmu);
+        }
+        
+        //taulukko lyhintä reittiä varten
+        int[] reitti = new int[lista.length];
+        
+        
+        while (checked[loppu]==false) { 
             
-            while (checked[loppu]==false) { 
-                Dijkstra nyk = keko.poll();
-                checked[nyk.id]=true;
-                
-                for (Dijkstra vierus: vl[nyk.id+1]) { 
-                    //relax
-                    if (nyk.dist+vierus.paino < vierus.dist) {
-                        vierus.dist = nyk.dist+vierus.paino;
+            Dijkstra nyk = keko.poll(); 
+            checked[nyk.id]=true; 
+            
+            //jokaiselle käsittelyssä olevan solmun vierussolmuille
+            for (Dijkstra vierus: vl[nyk.id+1]) { 
+               
+                if (nyk.dist+vierus.paino < vierus.dist) {
+                    vierus.dist = nyk.dist+vierus.paino;
+                    
+                    /* Koska keossa jo olevien solmujen arvoja ei voi muuttaa, lisätään kekoon
+                    uusi olio. Luodaan muutettava solmu uudestaan uudella distance-arvolla. */
+                    keko.add(new Dijkstra(vierus.id, vierus.paino, vierus.dist));
                         
-                        keko.add(new Dijkstra(vierus.id, vierus.paino, vierus.dist));
-                        
-                        reitti[vierus.id]=nyk.id;
-                    }
+                    reitti[vierus.id]=nyk.id; 
                 }
             }
+        }
 
-            tulostaReitti(reitti, alku, loppu);        
+        tulostaReitti(reitti, alku, loppu);        
+    }
+    
+
+    /* Tulostetaan lyhin reitti alkusolmusta loppusolmuun */
+    public static void tulostaReitti(int[] reitti, int alku, int loppu) {
+        
+        int u = reitti[loppu]; //u on sen solmun indeksi, josta viimeiseen solmuun kuljettiin
+
+        Stack<Integer> print = new Stack();
+        
+        //siirrytään reitti-taulukossa taaksepäin niin kauan, kunnes tullaan alkusolmuun
+        while (u != alku) { 
+            print.add(u);
+            u = reitti[u]; 
         }
         
-        public static void tulostaReitti(int[] reitti, int alku, int loppu) {
-            int u = reitti[loppu];
-
-            Stack<Integer> print = new Stack();
-            while (u != alku) { 
-                print.add(u);
-                u = reitti[u];
-
-            }
-            System.out.print(alku+" -> ");
-            while (!print.isEmpty()) {
-                u = print.pop();
-                System.out.print(u + " -> ");
-            }
-            System.out.println(loppu);
+        System.out.print(alku+" -> ");
+        
+        while (!print.isEmpty()) {
+            u = print.pop();
+            System.out.print(u + " -> ");
         }
-
+        System.out.println(loppu);
+    }
 }
--- lazy-route/src/main/java/lazyroute/Verkko.java
+++ lazy-route/src/main/java/lazyroute/Verkko.java
@@ -12,38 +12,70 @@ import java.util.ArrayList;
  * @author inkeriv
  */
 
-/*Käsiteltävän verkon luonti. Ei lopullinen verkkojen presentaatio.*/
-public class Verkko {
-    
-            public static void teeLista() {
-            Dijkstra[] lista = new Dijkstra[9];
-            lista[0]=new Dijkstra(0,3,100); lista[1]=new Dijkstra(1,4,100);
-            lista[2]=new Dijkstra(2,5,100); lista[3]=new Dijkstra(3,4,100);
-            lista[4]=new Dijkstra(4, 6, 100); lista[5]=new Dijkstra(5, 4, 100);
-            lista[6]=new Dijkstra(6, 3, 100); lista[7]=new Dijkstra(7, 3, 100);
-            lista[8]=new Dijkstra(8, 3, 100); 
-            
-            //vieruslista
-            ArrayList[] vl = new ArrayList[10];
-            for (int i=1; i<=lista.length; i++) {
-                vl[i]= new ArrayList<Dijkstra>(); 
-            }
-            vl[1].add(lista[1]); vl[1].add(lista[3]); 
-            vl[2].add(lista[0]); vl[2].add(lista[4]); vl[2].add(lista[2]);
-            vl[3].add(lista[1]); vl[3].add(lista[5]); 
-            vl[4].add(lista[0]); vl[4].add(lista[4]); vl[4].add(lista[6]);
-            vl[5].add(lista[1]); vl[5].add(lista[3]); vl[5].add(lista[5]); vl[5].add(lista[7]);
-            vl[6].add(lista[2]); vl[6].add(lista[4]); vl[6].add(lista[8]);
-            vl[7].add(lista[3]); vl[7].add(lista[7]);
-            vl[8].add(lista[6]); vl[8].add(lista[4]); vl[8].add(lista[8]);
-            vl[9].add(lista[5]); vl[9].add(lista[7]);
 
-            int a=0;
-            int l=8;
-            Dijkstra.dijkstra(lista, vl, a, l);    
+public class Verkko {
+     
+    /* Verkon luonti. Tällä hetkellä vain Dijkstran algoritmille.
+    
+    Dijkstran toimivuutta kokeillakseni verkko on vielä suoraa valmiina kirjoitettuna ilman
+    kummallisempia luomismetodeja. Verkko on m*n ruudukko, jossa solmujen paino kuvaa hankaluutta
+    siirtyä kyseiseen solmuun. Solmut ovat Dijkstra-olioita joilla id(=listojen indeksi), 
+    paino(= ns. kaaren paino / kulun hankaluus) ja dist(=dijkstran tarvitsema etäisyys alkusolmusta).
+    
+    Koska verkko on ruudukko, vierussolmut menee niin, että kulmilla on kaksi vierusta, reunasolmuilla
+    kolme ja muilla 4. Tarkoitus olisi myöhemmin saada kirjoitettua sellaisen metodit, jotka saa vain
+    listan solmujen painoista ja alku- ja loppusolmun, ja metodi osaa itse tehdä oliot ja vieruslistan.
+    
+    Verkko (solmujen painot):               Verkko (solmujen järjestys):
+    3   -   4   -   5                       0   -   1   -   2
+    |       |       |                       |       |       |
+    4   -   6   -   4                       3   -   4   -   5    
+    |       |       |                       |       |       |
+    3   -   3   -   3                       6   -   7   -   8
+    
+    aloitussolmu on 0, painona 3
+    maalisolmu on 8, painona 3
+    
+    kevyin reitti on 0 -> 3 -> 6 -> 7 -> 8
+    
+    */
+    
+    public static void teeLista() {
+        
+        /* Solmu-oliot listaan */
+        
+        Dijkstra[] lista = new Dijkstra[9];
+        lista[0]=new Dijkstra(0,3,100); lista[1]=new Dijkstra(1,4,100);
+        lista[2]=new Dijkstra(2,5,100); lista[3]=new Dijkstra(3,4,100);
+        lista[4]=new Dijkstra(4, 6, 100); lista[5]=new Dijkstra(5, 4, 100);
+        lista[6]=new Dijkstra(6, 3, 100); lista[7]=new Dijkstra(7, 3, 100);
+        lista[8]=new Dijkstra(8, 3, 100); 
+            
+        
+        /* Vieruslistan luonti */
+        
+        ArrayList[] vl = new ArrayList[10];
+        for (int i=1; i<=lista.length; i++) {
+            vl[i]= new ArrayList<Dijkstra>(); 
         }
+        vl[1].add(lista[1]); vl[1].add(lista[3]); 
+        vl[2].add(lista[0]); vl[2].add(lista[4]); vl[2].add(lista[2]);
+        vl[3].add(lista[1]); vl[3].add(lista[5]); 
+        vl[4].add(lista[0]); vl[4].add(lista[4]); vl[4].add(lista[6]);
+        vl[5].add(lista[1]); vl[5].add(lista[3]); vl[5].add(lista[5]); vl[5].add(lista[7]);
+        vl[6].add(lista[2]); vl[6].add(lista[4]); vl[6].add(lista[8]);
+        vl[7].add(lista[3]); vl[7].add(lista[7]);
+        vl[8].add(lista[6]); vl[8].add(lista[4]); vl[8].add(lista[8]);
+        vl[9].add(lista[5]); vl[9].add(lista[7]);
+
+        int a=0; //alkusolmu
+        int l=8; //loppusolmu
+        
+        // Kutsutaan dijkstraa, joka saa listan olioista, vieruslistan ja alku- ja loppusolmun
+        Dijkstra.dijkstra(lista, vl, a, l);    
+    }
               
-        public static void main(String[] args) {
-            teeLista();
-        } 
+    public static void main(String[] args) {
+        teeLista();
+    } 
 }
